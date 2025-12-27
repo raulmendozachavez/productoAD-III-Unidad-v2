14c99ff1699b822b58256c9d24ae004ae4dc9dbb
Raul Mendoza Chavez <raulandremendozachavez@gmail.com>
2025-12-26 21:08:53 -0500

    Correccion de ruta para background en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sanando Huellitas</title>
     <link rel="stylesheet" href="{{ asset('css/auth-styles.css') }}">
-    <body class="login-body">
 </head>
-<body>
+<body class="login-body">
     <header class="header">
         <div class="container">
             <div class="logo">
